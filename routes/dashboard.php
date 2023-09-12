30f8792072d289fc4d9db62cc493ef629701b4f5
unknown <gamalelhalwany3@gmail.com>
2023-09-12 04:25:32 +0300

    made new changes on categories

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -5,7 +5,10 @@ use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 
+Route::group(['middleware' => 'auth'], function () {
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
-Route::resource('dashboard/categories', CategoriesController::class);
+    Route::resource('dashboard/categories', CategoriesController::class);
+});
+
