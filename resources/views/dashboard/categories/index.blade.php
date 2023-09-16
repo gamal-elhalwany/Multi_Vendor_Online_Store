23ef77c701f4cee4cc2943ec757e6b9017419ecf
unknown <gamalelhalwany3@gmail.com>
2023-09-16 04:23:32 +0300

    Some Modifications

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -9,62 +9,63 @@
 
 @section('content')
 
-<x-alert type="success" />
+    <x-alert type="success" />
 
-<form action="{{ URL::current() }}" method="get" class="d-flex justify-content-between mb-4">
-    <input type="text" name="name" class="form-control mx-2" placeholder="Filter Categories" :value="request('name')">
-    <select name="status" class="form-control mx-2">
-        <option value="">All</option>
-        <option value="active" @selected(request('status') == 'active')>Active</option>
-        <option value="archived" @selected(request('status') == 'archived')>Archived</option>
-    </select>
-    <button type="submit" class="btn btn-dark mx-2 form-control">Search</button>
-</form>
+    <form action="{{ URL::current() }}" method="get" class="d-flex justify-content-between mb-4">
+        <input type="text" name="name" class="form-control mx-2" placeholder="Filter Categories" :value="request('name')">
+        <select name="status" class="form-control mx-2">
+            <option value="">All</option>
+            <option value="active" @selected(request('status') == 'active')>Active</option>
+            <option value="archived" @selected(request('status') == 'archived')>Archived</option>
+        </select>
+        <button type="submit" class="btn btn-dark mx-2 form-control">Search</button>
+    </form>
 
-<table class="table">
-    <a href="{{ route('categories.create') }}" class="btn btn-outline-primary m-3">Create Category</a>
-    <thead>
-        <tr>
-            <th>Image</th>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Parent_Id</th>
-            <th>Status</th>
-            <th>Created At</th>
-            <th colspan="2">Actions</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ($categories as $category)
-        <tr>
-            <td>
-                <img src="{{ asset('storage/' .$category->image) }}" alt="Image" height="50">
-            </td>
-            <td>{{ $category->id }}</td>
-            <td>{{ $category->name }}</td>
-            <td>{{ $category->parent_id }}</td>
-            <td>{{ $category->status }}</td>
-            <td>{{ $category->created_at }}</td>
-            <td>
-                <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-outline-primary btn-sm">Edit</a>
-            </td>
-            <td>
-                <form action="{{ route('categories.destroy', $category->id) }}" method="post">
-                    @csrf
-                    @method('delete')
-                    <button type="submit" class="btn btn-outline-danger btn-sm">Delete</button>
-                </form>
-            </td>
-        </tr>
-        @empty
-        <tr>
-            <td>No Categories Defined.</td>
-        </tr>
-        @endforelse
-    </tbody>
-</table>
+    <table class="table">
+        <a href="{{ route('categories.create') }}" class="btn btn-outline-primary m-3">Create Category</a>
+        <thead>
+            <tr>
+                <th>Image</th>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Parent_Id</th>
+                <th>Status</th>
+                <th>Created At</th>
+                <th colspan="2">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @forelse ($categories as $category)
+                <tr>
+                    <td>
+                        <img src="{{ asset('storage/' . $category->image) }}" alt="Image" height="50">
+                    </td>
+                    <td>{{ $category->id }}</td>
+                    <td>{{ $category->name }}</td>
+                    <td>{{ $category->parent_id }}</td>
+                    <td>{{ $category->status }}</td>
+                    <td>{{ $category->created_at }}</td>
+                    <td>
+                        <a href="{{ route('categories.edit', $category->id) }}"
+                            class="btn btn-outline-primary btn-sm">Edit</a>
+                    </td>
+                    <td>
+                        <form action="{{ route('categories.destroy', $category->id) }}" method="post">
+                            @csrf
+                            @method('delete')
+                            <button type="submit" class="btn btn-outline-danger btn-sm">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @empty
+                <tr>
+                    <td>No Categories Defined.</td>
+                </tr>
+            @endforelse
+        </tbody>
+    </table>
 
-{{-- If your app uses a bootstrap as css framework and this method doesn't work with you go to the appServiceProvider and call for this method:Paginator::useBootstrap(); --}}
-{{ $categories->withQueryString()->links() }}
+    {{-- If your app uses a bootstrap as css framework and this method doesn't work with you go to the appServiceProvider and call for this method:Paginator::useBootstrap(); --}}
+    {{ $categories->withQueryString()->links() }}
 
 @endsection
