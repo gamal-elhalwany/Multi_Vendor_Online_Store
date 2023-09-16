b9ac46a56c23807b63656a3073c06359396cd6ab
unknown <gamalelhalwany3@gmail.com>
2023-09-16 04:48:18 +0300

    Creating nav links for categories

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -11,7 +11,7 @@
 
     <x-alert type="success" />
 
-    <form action="{{ URL::current() }}" method="get" class="d-flex justify-content-between mb-4 hello">
+    <form action="{{ URL::current() }}" method="get" class="d-flex justify-content-between mb-4">
         <input type="text" name="name" class="form-control mx-2" placeholder="Filter Categories" :value="request('name')">
         <select name="status" class="form-control mx-2">
             <option value="">All</option>
