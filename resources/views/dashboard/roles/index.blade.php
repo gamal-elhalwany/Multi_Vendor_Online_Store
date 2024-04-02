583b318456d04ee54fd4037be3cb804b60f21773
unknown <gamalelhalwany3@gmail.com>
2024-04-02 06:06:47 +0200

    Modified the dashboard nav-bar links

--- resources/views/dashboard/roles/index.blade.php
+++ resources/views/dashboard/roles/index.blade.php
@@ -3,8 +3,8 @@
 @section('title', 'Role Management')
 
 @section('breadcrumb')
-    @parent
-    <li class="breadcrumb-item active">Role Management</li>
+@parent
+<li class="breadcrumb-item active">Role Management</li>
 @endsection
 
 
@@ -15,7 +15,7 @@
             <h2>Role Management</h2>
         </div>
         <div class="pull-right">
-        @can('role-create')
+            @can('role-create')
             <a class="btn btn-success" href="{{ route('roles.create') }}"> Create New Role </a>
             @endcan
         </div>
@@ -24,31 +24,31 @@
 
 
 @if ($message = Session::get('success'))
-    <div class="alert alert-success">
-        <p>{{ $message }}</p>
-    </div>
+<div class="alert alert-success">
+    <p>{{ $message }}</p>
+</div>
 @endif
 
 
 <table class="table table-bordered">
-  <tr>
-     <th>No</th>
-     <th>Name</th>
-     <th width="280px">Action</th>
-  </tr>
+    <tr>
+        <th>No</th>
+        <th>Name</th>
+        <th width="280px">Action</th>
+    </tr>
     @foreach ($roles as $key => $role)
     <tr>
         <td>{{ ++$i }}</td>
         <td>{{ $role->name }}</td>
         <td>
             <a class="btn btn-info" href="{{ route('roles.show',$role->id) }}">Show</a>
-            @can('role-edit')
-                <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
+            @can('edit-role')
+            <a class="btn btn-primary" href="{{ route('roles.edit',$role->id) }}">Edit</a>
             @endcan
-            @can('role-delete')
-                {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
-                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
-                {!! Form::close() !!}
+            @can('delete-role')
+            {!! Form::open(['method' => 'DELETE','route' => ['roles.destroy', $role->id],'style'=>'display:inline']) !!}
+            {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+            {!! Form::close() !!}
             @endcan
         </td>
     </tr>
