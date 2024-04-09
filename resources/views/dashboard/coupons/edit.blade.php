ef28f2ef805249b0a3ebe7d6d2ec10a246e32ae2
gamal-elhalwany <gamalelhalwany3@gmail.com>
2024-04-09 04:47:33 +0200

    Working on coupons system. Done 92%

--- resources/views/dashboard/coupons/edit.blade.php
+++ resources/views/dashboard/coupons/edit.blade.php
@@ -34,7 +34,7 @@
 
 <form action="{{ route('coupons.update', $coupon->id) }}" method="POST">
     @csrf
-    @method('PATCH')
+    @method('PUT')
     <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-6">
             <div class="form-group">
