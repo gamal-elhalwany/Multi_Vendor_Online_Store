0625dc9eb0bd620f460cf0afb5da6f23d0c179a4
gamal-elhalwany <gamalelhalwany3@gmail.com>
2024-06-22 06:21:13 +0300

    Working on Product Options.

--- resources/views/dashboard/products/show.blade.php
+++ resources/views/dashboard/products/show.blade.php
@@ -1,1 +1,8 @@
 <h3>Here's the show product page, welcome {{ auth()->user()->name }}</h3>
+
+@foreach($options as $option)
+<div style="margin: 20px;">
+    <strong>{{ $option->name }}:</strong>
+    <span>{{ $option->value }}</span>
+</div>
+@endforeach
